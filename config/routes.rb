6d9a97b37a8115405b7eb566374e9ffa2bd1312c
gianmaria scioti <jimmyscioti@gmail.com>
2019-02-22 17:38:18 +0100

    cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :new, :show, :create] do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:new, :show, :create] do
     resources :doses, only: [:create, :new,]
   end
   resources :doses, only: :destroy
